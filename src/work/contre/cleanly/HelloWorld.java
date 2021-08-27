5a943980c26119996df2e3989d361ade5cfb4c18
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-27 22:55:01 +0900

    add 0-5

--- src/work/contre/cleanly/HelloWorld.java
+++ src/work/contre/cleanly/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 		System.out.println("奏くんは");
 		System.out.println("お酒が好き");
 		System.out.println("ではありません");
+		System.out.println("年齢は30 + 4才です");
+		System.out.println(30 + 4);
 	}
 }
