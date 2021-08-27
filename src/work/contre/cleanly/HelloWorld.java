54b7cc6061ce118655ee7e5d028b3ca15aea13f7
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-27 22:53:07 +0900

    add 0-4

--- src/work/contre/cleanly/HelloWorld.java
+++ src/work/contre/cleanly/HelloWorld.java
@@ -2,6 +2,8 @@ package work.contre.cleanly;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("奏でくん");
+		System.out.println("奏くんは");
+		System.out.println("お酒が好き");
+		System.out.println("ではありません");
 	}
 }
