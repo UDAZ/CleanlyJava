98b66e055635f2eaef491415ee37a75260515856
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-28 22:22:55 +0900

    add 1-2

---
+++ src/work/contre/cleanly/Hensu.java
@@ -0,0 +1,11 @@
+package work.contre.cleanly;
+
+public class Hensu {
+
+	public static void main(String[] args) {
+		int age; // 変数宣言の文(ageという箱を用意)
+		age = 30; // 箱の中に30を入れる
+		System.out.println(age);//箱の中を表示する
+	}
+
+}
