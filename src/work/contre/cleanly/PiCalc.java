2307a40576d5a27839be92cc7badc57218a38a39
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-31 00:10:18 +0900

    add 1-5

--- src/work/contre/cleanly/PiCalc.java
+++ src/work/contre/cleanly/PiCalc.java
@@ -3,17 +3,15 @@ package work.contre.cleanly;
 public class PiCalc {
 
 	public static void main(String[] args) {
-		double pi = 3.14;
+		// 定数を利用する。 final 型 定数名 = 初期値;
+		final double PI = 3.14;
 		int pie = 5;
 		System.out.println("半径" + pie + "cmのパイの面積は、");
-		System.out.println(pie * pie * pi);
+		System.out.println(pie * pie * PI);
 		System.out.println("パイの半径を倍にします");
 		
-		pi = 10; 
-		// ほんとうはpieに代入すべきだったのに...
-		System.out.println("半径" + pie + "cmのパイの面積は、");
-		System.out.println(pie * pie * pi);
-		// 変えられない値にしたいです
+		PI = 10;
+		// これで再利用できない。定数です！
 	}
 
 }
