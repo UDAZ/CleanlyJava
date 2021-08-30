b0e4dd28e725dff31ab421cb64a10593e729b6cd
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-30 23:58:36 +0900

    add 1-3

---
+++ src/work/contre/cleanly/Reuse.java
@@ -0,0 +1,14 @@
+package work.contre.cleanly;
+
+public class Reuse {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int age = 22;
+		System.out.println("年齢は"+ age);
+		age = 30;
+		System.out.println("うそです。ほんとは"+ age);
+		// 変数の再代入	
+	}
+
+}
