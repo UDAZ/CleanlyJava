089023139184512169cccc9283c95340f4e56bf7
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-30 23:55:19 +0900

    add 変数の初期化

---
+++ src/work/contre/cleanly/Age.java
@@ -0,0 +1,14 @@
+package work.contre.cleanly;
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// int age;
+		// age = 22;
+		// 変数宣言と値の代入を一緒に行うことを「変数の初期化」という。
+		int age = 22;
+		System.out.println(age);
+	}
+
+}
