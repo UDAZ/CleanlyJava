9a052817df1ad64a7a8c7be477ef00e6edd905c9
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-01 00:19:09 +0900

    update 2-7

--- src/work/contre/cleanly/ForceExchange.java
+++ src/work/contre/cleanly/ForceExchange.java
@@ -5,6 +5,7 @@ public class ForceExchange {
 	public static void main(String[] args) {
 		int age = (int)3.2; // 3.2をintに型変換して代入せよ
 		System.out.println(age);
+		// (キャスト演算子)はよほどのことがない限り使わない
 	}
 
 }
