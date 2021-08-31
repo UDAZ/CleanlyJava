dd9a03789d4e632d2e07d8831c1940d1b8476d34
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-01 00:17:23 +0900

    add 2-7

---
+++ src/work/contre/cleanly/ForceExchange.java
@@ -0,0 +1,10 @@
+package work.contre.cleanly;
+
+public class ForceExchange {
+
+	public static void main(String[] args) {
+		int age = (int)3.2; // 3.2をintに型変換して代入せよ
+		System.out.println(age);
+	}
+
+}
--- src/work/contre/cleanly/SmallerExchange.java
+++ src/work/contre/cleanly/SmallerExchange.java
@@ -3,7 +3,7 @@ package work.contre.cleanly;
 public class SmallerExchange {
 
 	public static void main(String[] args) {
-		int i = 3.2;
+		// int i = 3.2;
 		// double から int には変換できません
 	}
 
