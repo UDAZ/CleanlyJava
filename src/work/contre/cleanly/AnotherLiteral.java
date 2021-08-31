4fb77205390cfcdb7a9a9d752467c835fe732f12
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-01 00:24:47 +0900

    add 2-8

---
+++ src/work/contre/cleanly/AnotherLiteral.java
@@ -0,0 +1,12 @@
+package work.contre.cleanly;
+
+public class AnotherLiteral {
+
+	public static void main(String[] args) {
+		double d = 8.5 / 2; // 2を2.0に変換している
+		long l = 5 + 2L; // 5を5Lに変換している
+		System.out.println(d);
+		System.out.println(l);
+	}
+
+}
