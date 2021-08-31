db321813a72054786262ed7147a0a12b55ce376f
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-31 23:49:53 +0900

    add 2-1

---
+++ src/work/contre/cleanly/CalcString.java
@@ -0,0 +1,15 @@
+package work.contre.cleanly;
+
+public class CalcString {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a; // 変数宣言
+		int b; // 変数宣言
+		a = 20; // 計算の文（代入）
+		b = a + 5; // 計算文(足し算して代入)
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
