45b99bd5977a4f4907d543917937514dbfe9e48a
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-01 00:46:48 +0900

    add 2-12

---
+++ src/work/contre/cleanly/MathMax.java
@@ -0,0 +1,12 @@
+package work.contre.cleanly;
+
+public class MathMax {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int m = Math.max(a, b); // 2つの値を比較して大きい方の数値を代入する。
+		System.out.println("比較実験結果: " + a + "と" + b + "とで大きいほうは、"+ m);
+	}
+
+}
