ff178edbe0a315b21badae7b7a66233a92289d6a
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-01 00:36:20 +0900

    add 2-10

---
+++ src/work/contre/cleanly/CommandString.java
@@ -0,0 +1,12 @@
+package work.contre.cleanly;
+
+public class CommandString {
+
+	public static void main(String[] args) {
+		String name = "すがわら"; // 変数宣言文
+		String message;// 変数宣言文
+		message = name + "さん、こんにちは！"; // 計算文
+		System.out.println(message); // 命令実行文
+	}
+
+}
