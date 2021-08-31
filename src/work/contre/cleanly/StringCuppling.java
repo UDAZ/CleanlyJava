631da4e92f4165ab98cf2b9f45d269eb0f3b147f
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-01 00:28:39 +0900

    add 2-9

---
+++ src/work/contre/cleanly/StringCuppling.java
@@ -0,0 +1,11 @@
+package work.contre.cleanly;
+
+public class StringCuppling {
+
+	public static void main(String[] args) {
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
+		// 23(int型)がString型に変換されて連結
+	}
+
+}
