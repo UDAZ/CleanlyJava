597bdd162bc1b02718117e7fd6372efe048217e7
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-31 23:54:34 +0900

    add 2-3

--- src/work/contre/cleanly/EscapeSequence.java
+++ src/work/contre/cleanly/EscapeSequence.java
@@ -3,8 +3,8 @@ package work.contre.cleanly;
 public class EscapeSequence {
 
 	public static void main(String[] args) {
-		System.out.println("私の好きな記号は二重引用符(")です");
-		// これだとダメ
+		System.out.println("私の好きな記号は二重引用符(\")です");
+		// これでOK
 	}
 
 }
