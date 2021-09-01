3f16e770bcbffc6314cfef433b8937a743fa2d10
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-02 01:08:36 +0900

    add 3-3

--- src/work/contre/cleanly/WhetherIf.java
+++ src/work/contre/cleanly/WhetherIf.java
@@ -7,9 +7,9 @@ public class WhetherIf {
 		if(tenki == true) {
 			System.out.println("洗濯をします");
 			System.out.println("散歩にいきます");
-		} else {
-			System.out.println("DVDを見ます");
-		}
+		} else 
+		System.out.println("DVDを見ます");
+		// {}のブロックが無くても1行だったら問題ないが実際の開発現場ではミス防止のため通常使う
 	}
 
 }
