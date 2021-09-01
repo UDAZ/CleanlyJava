bc56a63f41b56e360d9770daadaf6ad0350ffa4e
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-02 00:30:07 +0900

    add Lesson2-2

---
+++ src/work/contre/cleanly/Lesson2_2.java
@@ -0,0 +1,16 @@
+package work.contre.cleanly;
+
+public class Lesson2_2 {
+
+	public static void main(String[] args) {
+		// double からintには変換できない int x = 3 + 5.0;
+		double d = 2.0F;
+		// Stringからintには変換できない int number = "5";
+		String s = 2 + "人目";
+		byte b = 1;
+		// booleanからdoubleには変換できない double db = true;
+		short sb = (byte)2;
+		
+	}
+
+}
