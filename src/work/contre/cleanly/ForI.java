0a2bac1e5f8ee7edea9a632f6bbc75fb25504ca1
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-02 16:45:39 +0900

    add 3-8

---
+++ src/work/contre/cleanly/ForI.java
@@ -0,0 +1,11 @@
+package work.contre.cleanly;
+
+public class ForI {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print("現在" + (i+1) + "周目→");
+		}
+	}
+
+}
