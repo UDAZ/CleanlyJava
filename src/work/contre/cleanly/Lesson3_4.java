6b1b2d59d939fb4c88c141dc1d444d00e7ccce44
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-02 23:50:04 +0900

    add Lesson3-4

---
+++ src/work/contre/cleanly/Lesson3_4.java
@@ -0,0 +1,16 @@
+package work.contre.cleanly;
+
+public class Lesson3_4 {
+
+	public static void main(String[] args) {
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+
+}
