9df81a89a1ea7e07acf9b693e912fe64d8ebed92
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-02 17:34:45 +0900

    add Lesson3-1

---
+++ src/work/contre/cleanly/Lesson3_1.java
@@ -0,0 +1,25 @@
+package work.contre.cleanly;
+
+public class Lesson3_1 {
+
+	public static void main(String[] args) {
+		int weight = 60;
+		if(weight == 60) {
+			System.out.println("OK");
+		}
+		int age1 = 16;
+		int age2 = 15;
+		if ((age1 + age2)* 2 > 60) {
+			System.out.println("OK");
+		}
+		int age = 3;
+		if (age % 2 == 1) {
+			System.out.println("OK");
+		}
+		String name = "総";
+		if (name.equals("総")) {
+			System.out.println("OK");
+		}
+	}
+
+}
