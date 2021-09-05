2578a0d31e67aff6f9d9f98c35c141afbeddc45a
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-05 23:07:24 +0900

    add 4-7

--- src/work/contre/cleanly/NotInit.java
+++ src/work/contre/cleanly/NotInit.java
@@ -3,8 +3,13 @@ package work.contre.cleanly;
 public class NotInit {
 
 	public static void main(String[] args) {
-		int x;
-		System.out.println(x);
+		// ↓初期化されていないのでエラーになる
+		// int x;
+		// System.out.println(x);
+		
+		// ↓自動的に初期化されるのでエラーにならない
+		int[] scores = new int[5];
+		System.out.println(scores[0]);
 	}
 
 }
