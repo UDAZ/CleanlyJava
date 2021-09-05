24a64003ba18f1d6b5d65640db00dd10ed78662f
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-06 04:00:47 +0900

    update 4-19

--- src/work/contre/cleanly/ParentChild.java
+++ src/work/contre/cleanly/ParentChild.java
@@ -3,9 +3,9 @@ package work.contre.cleanly;
 public class ParentChild {
 
 	public static void main(String[] args) {
-		int[][] scores = {{40,50,60},{80,60,70}};
-		System.out.println(scores.length);
-		System.out.println(scores[0].length);
+		int[][] scores = {{40,50,60},{80,60,70}}; // このような 
+		System.out.println(scores.length); // parent
+		System.out.println(scores[0].length); // child
 	}
 
 }
