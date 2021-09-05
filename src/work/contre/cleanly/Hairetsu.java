10cce24eacb99fcc692eaca6c64f7c841e1aef19
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-05 22:53:40 +0900

    add 4-2

---
+++ src/work/contre/cleanly/Hairetsu.java
@@ -0,0 +1,10 @@
+package work.contre.cleanly;
+
+public class Hairetsu {
+
+	public static void main(String[] args) {
+		int[] scores; // 配列変数scoresを作成
+		scores = new int[5]; // scoresに5つの要素を作成
+	}
+
+}
