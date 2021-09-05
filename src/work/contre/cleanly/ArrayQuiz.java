048b6f24e9ddb29c82235a10f636c4c9ca106e39
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-06 03:17:03 +0900

    add 4-15

---
+++ src/work/contre/cleanly/ArrayQuiz.java
@@ -0,0 +1,14 @@
+package work.contre.cleanly;
+
+public class ArrayQuiz {
+
+	public static void main(String[] args) {
+		int[] arrayA = {1, 2, 3};
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
+		
+	}
+
+}
