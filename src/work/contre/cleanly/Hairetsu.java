89eb43606020b4af95f04266cad9592dc6911349
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-05 22:55:44 +0900

    add 4-3

--- src/work/contre/cleanly/Hairetsu.java
+++ src/work/contre/cleanly/Hairetsu.java
@@ -3,8 +3,11 @@ package work.contre.cleanly;
 public class Hairetsu {
 
 	public static void main(String[] args) {
-		int[] scores; // 配列変数scoresを作成
-		scores = new int[5]; // scoresに5つの要素を作成
+		/*
+		 * 	int[] scores;
+		 * 	scores = new int[5];
+		 */
+		int[] scores = new int[5];
 	}
 
 }
