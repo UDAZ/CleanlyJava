3efc391211fbc26c1138093651e22a7693c4ad98
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-06 02:37:43 +0900

    add 4-10

--- src/work/contre/cleanly/PointMS.java
+++ src/work/contre/cleanly/PointMS.java
@@ -3,15 +3,12 @@ package work.contre.cleanly;
 public class PointMS {
 
 	public static void main(String[] args) {
-		int sansu = 20;
-		int kokugo = 30;
-		int rika = 40;
-		int eigo = 50;
-		int syakai = 80;
-		
-		int sum = sansu + kokugo + rika + syakai + eigo;
-		
-		int avg = sum /5;
+		int[] scores = {20, 30, 40, 50, 80};
+		int sum = 0;
+		for (int i = 0; i < scores.length; i++) {
+			sum += scores[i];
+		}
+		int avg = sum / scores.length;
 		System.out.println("合計点：" + sum);
 		System.out.println("平均点：" + avg);
 	}
