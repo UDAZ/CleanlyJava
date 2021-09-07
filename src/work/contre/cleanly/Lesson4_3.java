ace25e0aa8ae9836ba6ff0251bd953f45669b19c
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-07 21:16:33 +0900

    update Lesson4-3

--- src/work/contre/cleanly/Lesson4_3.java
+++ src/work/contre/cleanly/Lesson4_3.java
@@ -5,7 +5,11 @@ public class Lesson4_3 {
 	public static void main(String[] args) {
 		int[] counts = null;
 		float[] heights = {171.3F,175.0F};
+		
+		// null なので参照できない
 		System.out.println(counts[1]); // NullPointerException
+		
+		// 2はないので参照できない
 		System.out.println(heights[2]); // ArrayIndexOutOfBoundsException
 	}
 
