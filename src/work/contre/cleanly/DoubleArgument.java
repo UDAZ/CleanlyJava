379e30e42546b13b9331daf07eca98f76b9541a2
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-08 01:04:55 +0900

    add 5-5

---
+++ src/work/contre/cleanly/DoubleArgument.java
@@ -0,0 +1,15 @@
+package work.contre.cleanly;
+
+public class DoubleArgument {
+
+	public static void main(String[] args) {
+		add(100, 20);
+		add(200, 50);
+	}
+	
+	// 複数の値を受け取るaddメソッド
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + " + " + y + " = " + ans);
+	}
+}
