7cbb9253e11066cddeeaa5d1137e09acf5223091
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-07 21:33:59 +0900

    add 5-2

--- src/work/contre/cleanly/HelloMethod.java
+++ src/work/contre/cleanly/HelloMethod.java
@@ -1,7 +1,11 @@
 package work.contre.cleanly;
 
 public class HelloMethod {
-
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
 	public static void hello() {
 		System.out.println("奏さん、こんにちは！");
 	}
