3fcb4336f83c4e749c61d44ebb96ba24e1433739
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-07 21:03:46 +0900

    add Lesson4-1

---
+++ src/work/contre/cleanly/Lesson4_1.java
@@ -0,0 +1,12 @@
+package work.contre.cleanly;
+
+public class Lesson4_1 {
+
+	public static void main(String[] args) {
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+
+}
