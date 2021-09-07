99d5ec89ad324aebf0bb54a9b7e22c0f668a6e1d
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-07 21:23:06 +0900

    add Lesson4_4

---
+++ src/work/contre/cleanly/Lesson4_4.java
@@ -0,0 +1,21 @@
+package work.contre.cleanly;
+
+public class Lesson4_4 {
+
+	public static void main(String[] args) {
+		int[] numbers = {3,4,9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int n : numbers) {
+			if(n == input) {
+				System.out.println("アタリ！");
+			}
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			if(numbers[i] == input) {
+				System.out.println("当たり！");
+			}
+		}
+	}
+
+}
