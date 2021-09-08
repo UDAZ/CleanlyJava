298e6b04d89a8595c18f2468a3d16c7298384fe1
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-08 22:32:27 +0900

    add 5-6

---
+++ src/work/contre/cleanly/ReturnError.java
@@ -0,0 +1,15 @@
+package work.contre.cleanly;
+
+public class ReturnError {
+
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 10;
+		add();
+	}
+	public static void add() {
+		int ans = x + y;
+		System.out.println(x+ "+" + y+"=" + ans);
+	}
+
+}
