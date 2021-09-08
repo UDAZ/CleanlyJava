3317a37099f785dae34e7ae282103d3f6977c83b
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-08 22:37:50 +0900

    add 5-8

---
+++ src/work/contre/cleanly/JustReturn.java
@@ -0,0 +1,12 @@
+package work.contre.cleanly;
+
+public class JustReturn {
+
+	public static void main(String[] args) {
+		System.out.println(add(add(10,30),add(30,40))); // (10+30) + (30+40) = 110
+	}
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+	}
+}
