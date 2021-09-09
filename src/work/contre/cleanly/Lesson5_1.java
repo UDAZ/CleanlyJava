0762a38d9d997cf444cc232523a82308f7946e2d
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-09 18:04:26 +0900

    add Lesson5-1

---
+++ src/work/contre/cleanly/Lesson5_1.java
@@ -0,0 +1,20 @@
+package work.contre.cleanly;
+
+public class Lesson5_1 {
+	
+	public static void introduceOneSelf() {
+		String name = "吉田";
+		int age = 13;
+		double height = 154.4;
+		char eto = '寅';
+		System.out.println("私の名前は" + name);
+		System.out.println("私の年齢は" + age);
+		System.out.println("私の身長は" + height);
+		System.out.println("私の干支は" + eto);
+	}
+	
+	public static void main(String[] args) {
+		introduceOneSelf();
+	}
+
+}
