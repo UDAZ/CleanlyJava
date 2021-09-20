55102318066d1e4e472093b663242ccffb348b91
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-21 00:08:26 +0900

    update 6-10

--- src/work/contre/cleanly/Api.java
+++ src/work/contre/cleanly/Api.java
@@ -3,8 +3,10 @@ package work.contre.cleanly;
 public class Api {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int[] heights = {172, 149, 152, 191, 155};
+		java.util.Arrays.sort(heights);
+		for (int h : heights) {
+			System.out.println(h);
+		}
 	}
-
 }
