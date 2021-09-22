f1c3243a1d540207eaddcf53d3e11df8b32f4dfe
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-22 18:22:22 +0900

    update Lesson 6-1

--- src/work/contre/cleanly/Lesson6_1.java
+++ src/work/contre/cleanly/Lesson6_1.java
@@ -1,25 +1,13 @@
 package work.contre.cleanly;
 
+import work.contre.cleanly.comment.Zenhan;
+
 public class Lesson6_1 {
 
 	public static void main(String[] args) {
-		doWarusa();
-		doTogame();
-		callDeae();
-		showMondokoro();
-	}
-	public static void doWarusa() {
-		System.out.println("きなこでござる。食えませんがの。");
-	}
-	public static void doTogame() {
-		System.out.println("この老いぼれの目はごまかせませんぞ");
-	}
-	public static void callDeae() {
-		System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
-	}
-	public static void showMondokoro() {
-		System.out.println("飛車さん、角さん。もういいでしょう。");
-		System.out.println("この紋所が目に入らぬか！");
-		doTogame();
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		work.contre.cleanly.comment.Kouhan.callDeae();
+		work.contre.cleanly.comment.Kouhan.showMondokoro();
 	}
 }
