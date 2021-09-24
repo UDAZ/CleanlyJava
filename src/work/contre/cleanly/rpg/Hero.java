bf69d5add8da6f493e9373d40d1c617ea8f6e7c7
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-24 20:18:38 +0900

    add 8-2

---
+++ src/work/contre/cleanly/rpg/Hero.java
@@ -0,0 +1,11 @@
+package work.contre.cleanly.rpg;
+
+public class Hero {
+	String name;
+	int hp;
+	public void attack() {}
+	public void sleep() {}
+	public void sit(int sec) {}
+	public void slip() {}
+	public void run() {}
+}
