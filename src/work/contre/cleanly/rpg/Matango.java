1b2d478538ac4590c43c2755877b10cf7cdaedff
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-26 00:12:29 +0900

    add 8-15

--- src/work/contre/cleanly/rpg/Matango.java
+++ src/work/contre/cleanly/rpg/Matango.java
@@ -4,4 +4,8 @@ public class Matango {
 	int hp;
 	// int level = 10; // お化けキノコのレベルに初期値10を設定した。
 	final int LEVEL = 10; // 定数でLEVELは10に固定
+	char suffix;
+	public void run() {
+		System.out.println("お化けキノコ" + suffix + "は逃げ出した！");
+	}
 }
