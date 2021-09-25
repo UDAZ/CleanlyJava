84f3e858b1e8c25abed71529ccf18c37dc0bfe1c
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-26 00:00:46 +0900

    add 8-12

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -12,7 +12,10 @@ public class Main {
 		 */
 		// 1. 勇者を生成
 		Hero h = new Hero(); // Heroクラスからインスタンスを生成し、変数hに入れる。
-		
+		// 2. フィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者【" + h.name + "】を生み出しました！");
 	}
 
 }
