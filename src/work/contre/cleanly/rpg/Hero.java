f77cf2d3338e2ba4fa29e896cc3480884d4e6fb4
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-25 19:00:36 +0900

    add 8-7

--- src/work/contre/cleanly/rpg/Hero.java
+++ src/work/contre/cleanly/rpg/Hero.java
@@ -3,4 +3,16 @@ package work.contre.cleanly.rpg;
 public class Hero {
 	String name; // 名前の宣言
 	int hp; // HPの宣言
+	
+	/*
+	 * 【名前】sleep
+	 * 【必要情報】なし
+	 * 【結果】なし
+	 * 【処理内容】眠ったあとはHPが100に回復する
+	 */
+	public void sleep() {
+		this.hp = 100; // 自分自身のhpフィールド
+		System.out.println(this.name + "は、眠って回復した！");
+		// thisとは特別に準備された変数で、「自分自身のインスタンス」を意味しています。
+	}
 }
