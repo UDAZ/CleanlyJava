a7fe408dd6d169760a5714ae9cf3176b517c38ad
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-25 18:54:32 +0900

    add 8-6

--- src/work/contre/cleanly/rpg/Matango.java
+++ src/work/contre/cleanly/rpg/Matango.java
@@ -2,5 +2,6 @@ package work.contre.cleanly.rpg;
 
 public class Matango {
 	int hp;
-	int level = 10; // お化けキノコのレベルに初期値10を設定した。
+	// int level = 10; // お化けキノコのレベルに初期値10を設定した。
+	final int LEVEL = 10; // 定数でLEVELは10に固定
 }
