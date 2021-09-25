23b3b1a31ed6c25587db741a1f45d4f12e226488
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-26 00:46:18 +0900

    add Lesson8-4

--- src/work/contre/cleanly/rpg/Cleric.java
+++ src/work/contre/cleanly/rpg/Cleric.java
@@ -6,10 +6,21 @@ public class Cleric {
 	final int MAX_HP = 50;
 	int mp = 10;
 	final int MAX_MP = 10;
+	
 	public void selfAid() {
 		System.out.println(this.name + "はセルフエイドを唱えた！");
 		this.mp -= 5;
 		this.hp = MAX_HP;
 		System.out.println("HPが最大まで回復した");
+	}
+	
+	public int pray(int sec) {
+		System.out.println(this.name + "は、"+ "秒間祈った！");
+		int recover = new java.util.Random().nextInt(3) + sec;
+		int recoverActual = Math.min(this.MAX_HP - this.mp, recover); // どちらか小さいほう、左が現在のMPから回復できる量で右がリカバーで回復できる量。
+		this.mp += recoverActual;
+		System.out.println("MPが"+ recoverActual + "回復した");
+		return recoverActual;
+		
 	}
 }
