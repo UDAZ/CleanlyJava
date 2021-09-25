b20eff761b1a6dace6188eb12dc599fa3f9cdc71
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-26 00:04:41 +0900

    add 8-13

--- src/work/contre/cleanly/rpg/Hero.java
+++ src/work/contre/cleanly/rpg/Hero.java
@@ -18,7 +18,7 @@ public class Hero {
 	
 	public void sit(int sec) {
 		this.hp += sec;
-		System.out.println(this.name + "は、" + sec + "座った。");
+		System.out.println(this.name + "は、" + sec + "秒座った。");
 		System.out.println("HPが" + sec + "ポイント回復した。");
 	}
 	
