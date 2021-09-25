3efe90c99453b7709591127b6df41a9769c5502f
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-26 00:29:03 +0900

    add Lesson8-3

--- src/work/contre/cleanly/rpg/Cleric.java
+++ src/work/contre/cleanly/rpg/Cleric.java
@@ -6,4 +6,10 @@ public class Cleric {
 	final int MAX_HP = 50;
 	int mp = 10;
 	final int MAX_MP = 10;
+	public void selfAid() {
+		System.out.println(this.name + "はセルフエイドを唱えた！");
+		this.mp -= 5;
+		this.hp = MAX_HP;
+		System.out.println("HPが最大まで回復した");
+	}
 }
