3b19a4b04611e0fc77fc11e160bc8f97a795493e
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-26 00:15:31 +0900

    add 8-16

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -12,14 +12,20 @@ public class Main {
 		 */
 		// 1. 勇者を生成
 		Hero h = new Hero(); // Heroクラスからインスタンスを生成し、変数hに入れる。
-		// 2. フィールドに初期値をセット
 		h.name = "ミナト";
 		h.hp = 100;
-		System.out.println("勇者【" + h.name + "】を生み出しました！");
-		// 3. 勇者のメソッドを呼び出していく
-		h.sit(5);
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m1.hp = 48;
+		m1.suffix = 'B';
+		
 		h.slip();
-		h.sit(25);
+		m1.run();
+		m2.run();
 		h.run();
 	}
 
