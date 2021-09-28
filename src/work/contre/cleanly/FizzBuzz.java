8c2d85b77e297e539a151e79a18e7fa5c884452f
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-09-29 01:57:08 +0900

    update FizzBuzz

--- src/work/contre/cleanly/FizzBuzz.java
+++ src/work/contre/cleanly/FizzBuzz.java
@@ -14,13 +14,19 @@ public class FizzBuzz {
 		int numBuzz = 5;
 		// Fizz 文字列
 		String strFizz = "Fizz";
+		// Buzz 文字列
 		String strBuzz = "Buzz";
+		// forで100回の繰り返し
 		for (i = 1; i <= loopCount; i++) {
+			// i で回数を表示
 			System.out.print(i + " ");
+			// FizzBuzzの判定　これは必ず最初
 			if (i % (numFizz * numBuzz) == 0) {
 				System.out.println(strFizz + strBuzz);
+			// Fizzの判定
 			} else if (i % numFizz == 0) {
 				System.out.println(strFizz);
+			// Buzzの判定
 			} else if (i % numBuzz == 0) {
 				System.out.println(strBuzz);
 			} else {
