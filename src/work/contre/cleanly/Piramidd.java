1686da16aae5e6ee2d7aae7b9520441e7a8d316b
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-01 00:32:59 +0900

    add piramidd

--- src/work/contre/cleanly/Piramidd.java
+++ src/work/contre/cleanly/Piramidd.java
@@ -4,7 +4,15 @@ public class Piramidd {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < (9 - i * 2); k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
