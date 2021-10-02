553e12cb422f7a63df03763322b195e0009f6932
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-02 23:12:31 +0900

    add 9-1

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -11,22 +11,10 @@ public class Main {
 		 * お化けキノコよ、逃げろ！
 		 */
 		// 1. 勇者を生成
-		Hero h = new Hero(); // Heroクラスからインスタンスを生成し、変数hに入れる。
-		h.name = "ミナト";
+		Hero h;
+		h = new Hero(); // Heroクラスからインスタンスを生成し、変数hに入れる。
 		h.hp = 100;
-		
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
-		
-		Matango m2 = new Matango();
-		m1.hp = 48;
-		m1.suffix = 'B';
-		
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
+
 	}
 
 }
