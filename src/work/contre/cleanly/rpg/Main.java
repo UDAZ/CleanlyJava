0ba7c665174eb07edbb578a14c832cca616e9813
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-02 23:57:13 +0900

    add 9-6

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -3,15 +3,18 @@ package work.contre.cleanly.rpg;
 public class Main {
 
 	public static void main(String[] args) {
-		Sword s = new Sword();
-		s.name = "炎の剣";
-		s.damage = 10;
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
-		h.sword = s;
-		System.out.println("現在の武器は" + h.sword.name);
-		h.attack();
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		Hero h2 = new Hero();
+		h2.name = "アサカ";
+		h2.hp = 100;
+		Wizzard w = new Wizzard();
+		w.name = "スガワラ";
+		w.hp = 50;
+		w.heal(h1); // ミナトを回復
+		w.heal(h2); // アサカを回復
+		w.heal(h2); // アサカを回復
 	}
 
 }
