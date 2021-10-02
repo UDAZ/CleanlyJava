dfeda4df6a3c51628c5f92b42d1b219c5952e40b
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-02 23:32:39 +0900

    add 9-2

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -3,18 +3,14 @@ package work.contre.cleanly.rpg;
 public class Main {
 
 	public static void main(String[] args) {
-		// (以下の内容をJavaで記述していく)
-		/*
-		 * 勇者よ、この仮想世界に生まれよ！
-		 * お化けキノコよ、この世界に生まれよ！
-		 * 勇者よ、戦え！
-		 * お化けキノコよ、逃げろ！
-		 */
-		// 1. 勇者を生成
-		Hero h; // 「Hero型のインスタンスだけを中に入れることができる」Hero型の箱を準備
-		h = new Hero(); // Heroインスタンス(メモリ領域)が生成され、変数hにHeroインスタンスが代入される
-		h.hp = 100; // 変数hからHeroインスタンスを参照し、hpフィールドを100に書き換える
-
+		Hero h1;
+		h1 = new Hero();
+		h1.hp = 100;
+		Hero h2;
+		h2 = h1;
+		h2.hp = 200;
+		System.out.println(h1.hp);
+		// h1とh2は同じHeroインスタンスを参照しているのでhpは200
 	}
 
 }
