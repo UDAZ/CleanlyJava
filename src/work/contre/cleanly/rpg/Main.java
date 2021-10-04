a07fa07a24984246f1c6414c3b6cc16951a32aeb
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-05 01:09:29 +0900

    add 10-6

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -5,7 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		SuperHero sh = new SuperHero();
 		sh.run();
-		sh.fly();
+		Hero h = new Hero();
+		h.run();
+		
+		// SuperHeroクラスのソースコードにはrunがないがインスタンス化されればrun()を呼び出すことができます。
+		// extendsを用いることによって差分だけを記述して新たなクラスを宣言することができます。
 	}
 
 }
