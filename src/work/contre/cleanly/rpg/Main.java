8cfbb6e651704c3724d3b4d5ef46258239488fea
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-04 23:01:40 +0900

    update 9-9

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Hero h = new Hero(); // インスタンスを生成して
 		System.out.println(h.hp);
+		// h.Hero(); のように開発者が直接呼び出すことはできない！JVMが実行しているから
 	}
 
 }
