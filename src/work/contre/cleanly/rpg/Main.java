73bb71c07a69dcea8f350978d97aad716913c175
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-05 00:36:04 +0900

    add 10-4

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -3,15 +3,9 @@ package work.contre.cleanly.rpg;
 public class Main {
 
 	public static void main(String[] args) {
-		Hero h1 = new Hero("ミナト"); // インスタンスを生成して
-		System.out.println(h1.name);
-		Hero h2 = new Hero();
-		System.out.println(h2.name);
-		System.out.println(h2.name);
-		// h.Hero(); のように開発者が直接呼び出すことはできない！JVMが実行しているから
-		// コンストラクタと見なされる条件
-		// ①メソッド名がクラス名と完全に等しい
-		// ②メソッド宣言に戻り値が記述されていない(voidもダメ)
+		SuperHero sh = new SuperHero();
+		sh.run();
+		sh.fly();
 	}
 
 }
