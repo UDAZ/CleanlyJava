f6fe58a0b6b503e4a110efaa8ffcc8460d8822d8
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-10-04 22:41:44 +0900

    add 9-7

--- src/work/contre/cleanly/rpg/Main.java
+++ src/work/contre/cleanly/rpg/Main.java
@@ -3,19 +3,8 @@ package work.contre.cleanly.rpg;
 public class Main {
 
 	public static void main(String[] args) {
-		Hero h1 = new Hero();
-		h1.name = "ミナト";
-		h1.hp = 100;
-		Hero h2 = new Hero();
-		h2.name = "アサカ";
-		h2.hp = 100;
-		Wizzard w = new Wizzard();
-		w.name = "スガワラ";
-		w.hp = 50;
-		w.heal(h1); // ミナトを回復
-		w.heal(h2); // アサカを回復
-		w.heal(h2); // アサカを回復
-		System.out.println(h2.hp);
+		String s = new String("こんにちは");
+		System.out.println(s);
 	}
 
 }
